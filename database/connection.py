4c5e37ed23a8f21ef036b96e1e2bb3350e5bb246
CBT MacBook <Christianbt96@gmail.com>
2024-12-05 12:17:07 +0100

    Added env variables

--- database/connection.py
+++ database/connection.py
@@ -1,13 +1,13 @@
 import sqlite3
 import os
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-#load_dotenv()
+load_dotenv()
 
-#SQLITE_DB_PATH = os.getenv("car_management.db")
+SQLITE_DB_PATH = os.getenv("/home/car_management.db")
 
 # Create or connect to SQLite database
 def create_connection():
-    connection = sqlite3.connect('car_management.db')
+    connection = sqlite3.connect('SQLite_DB_PATH')
     connection.row_factory = sqlite3.Row  # Rows as dictionaries
     return connection
