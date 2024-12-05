8f7e04fb800f8137bf0d02b751656f84777c646c
CBT MacBook <Christianbt96@gmail.com>
2024-12-05 12:22:06 +0100

    Fixed env path

--- database/connection.py
+++ database/connection.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLITE_DB_PATH = os.getenv("/home/car_management.db")
+SQLITE_DB_PATH = os.getenv('SQLITE_DB_PATH','/home/site/wwwroot/car_management.db')
 
 # Create or connect to SQLite database
 def create_connection():
-    connection = sqlite3.connect('SQLite_DB_PATH')
+    connection = sqlite3.connect(SQLITE_DB_PATH)
     connection.row_factory = sqlite3.Row  # Rows as dictionaries
     return connection
