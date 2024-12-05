fa55f6ccc93bdd0eca26712c81c17dc40de70903
marcus_rk <makj0005@stud.kea.dk>
2024-12-05 17:10:38 +0100

    Edited Dockerfile

--- database/connection.py
+++ database/connection.py
@@ -6,9 +6,6 @@ load_dotenv(override=True)
 
 SQLITE_DB_PATH = os.getenv('SQLITE_DB_PATH', os.path.join(os.environ['HOME'], 'data', 'car_management.db'))
 
-if not os.path.exists(SQLITE_DB_PATH):
-    raise FileNotFoundError(f"Database not found at {SQLITE_DB_PATH}")
-
 # Create or connect to SQLite database
 def create_connection():
     connection = sqlite3.connect(SQLITE_DB_PATH)
